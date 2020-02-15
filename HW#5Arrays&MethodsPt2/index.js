0a1969dd371c7817ba37205230561c44a26d8907
Anrdii <andnkt20@gmail.com>
2020-02-15 17:44:30 +0200

    make 2 example

--- HW#5Arrays&MethodsPt2/index.js
+++ HW#5Arrays&MethodsPt2/index.js
@@ -3,7 +3,7 @@ var numbers = [null, 2.3, 'str', 6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56,
 
 function filtCllbck(el){
 	return (el!== undefined && el!==null && typeof(el)!== 'string' && el%1==0);
-	//return  el%1==0;
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -16,27 +16,65 @@ getRandomArray = (length, min, max)=>{
 	return randArr;
 }
 
-console.log(getRandomArray(15,1,100));
+//console.log(getRandomArray(15,1,100));
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
 //2 kind a tuff maybe set or obj use or arr
-/*getModa = (...nums) => {
+//TODO: make it shorter
+let arrForCheck1 = [1,2,2,3,3,4,4,5];
+
+let arrForCheck2 = [...arrForCheck1, 5,5];
+
+getModa = (...nums) => {
+	let arrLen = nums.length;
 	let coins=[];
 	let p = nums.filter(filtCllbck);
-	p.forEach( el=> (p.filter(fEl=> fEl===el ).length>=2 )? coins.push(el): '')
+	//create arr of unique elements
+	let setOfNums = Array.from(new Set(p));
+
+	let maxOfCoins;
+
+	for (let i=0; i<setOfNums.length; i++){
+
+			let result = p.filter( el => el !==setOfNums[i]);
+	
+			let diffFilteNums =p.length-result.length;
+
+			if(i===0) {
+
+				maxOfCoins = diffFilteNums
+				coins.push(setOfNums[i]);//push coinsidence
+
+			}else{
+				//if now coins grater than what we have we should
+				if(maxOfCoins< diffFilteNums){
+					
+					maxOfCoins = diffFilteNums
+					//empty array
+					coins = [];
+					coins.push(setOfNums[i]);
+				
+				}else if( maxOfCoins == diffFilteNums){
+					coins.push(setOfNums[i]);
+				}
+			}
+		}
+			//console.log('[coins, maxOfCoins]',[coins, maxOfCoins])
 	return coins;
 }
-*/
 
-//console.log(getModa(...numbers))
+console.log(getModa(...numbers))
+
+console.log(getModa(...arrForCheck1))
+console.log(getModa(...arrForCheck2))
 
 //3
 getAverage = (...nums) =>   nums.filter(filtCllbck).reduce((a,b)=> a+b)/nums.filter(filtCllbck).length;
 
-console.log('Average',getAverage(...numbers));
+//console.log('Average',getAverage(...numbers));
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //4
 
 getMedian=(...nums) =>{
@@ -51,36 +89,36 @@ getMedian=(...nums) =>{
 let kuru = [7,4,5,3,1,2,6];
 let nunu = [1, 5, 3, 4, 2];
 let numu = [4, 3, 2, 1]
-console.log('Median of [1, 5, 3, 4, 2]',getMedian(...nunu));
-console.log('Median of [4, 3, 2, 1]' ,getMedian(...numu));
+//console.log('Median of [1, 5, 3, 4, 2]',getMedian(...nunu));
+//console.log('Median of [4, 3, 2, 1]' ,getMedian(...numu));
 //console.log(getMedian(...kuru));
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //5
 
 let exmplEvNums = [1, 2, 3, 4, 5, 6];
 
 filterEvenNumbers=(...nums) => nums.filter(filtCllbck).filter( num => num%2 > 0)
 
-console.log('Even nums for [1, 2, 3, 4, 5, 6]',filterEvenNumbers(...exmplEvNums));
+//console.log('Even nums for [1, 2, 3, 4, 5, 6]',filterEvenNumbers(...exmplEvNums));
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //6
 
 let arrForPosNums = [1, -2, 3, -4, -5, 6];
 
 countPositiveNumbers=(...numbers)=> numbers.filter( el=> el>0).length;
 
-console.log(countPositiveNumbers(...arrForPosNums));
+//console.log('Positive nums count-',countPositiveNumbers(...arrForPosNums));
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //7
 
 getDividedByFive = (...numbers) => numbers.filter(filtCllbck).filter( el => el%5 === 0 )
 
-console.log(getDividedByFive(...numbers))
+//console.log(getDividedByFive(...numbers))
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //8
 let strWithBadWrds = 'What a fuck you talking you Fucking fuck. Shitty-fuck';
 
@@ -109,10 +147,10 @@ replaceBadWords(strWithBadWrds, badTokens);
 replaceBadWords(songWithBadWords, songBadTokens);
 
 
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //9
 divideByThree=(word)=>{
-	console.log(word);
+
 	let str = word.toLowerCase().split(' ').join('').split('');
 
 	if(str.length>3){
@@ -125,20 +163,16 @@ divideByThree=(word)=>{
 			}
  		str = str.slice(1).join('').split(' ')
  		return str;
-	}	else return str.join('');
-	console.log('Arr',str);
+	}	else return word;
+	
 }
 
-console.log(divideByThree('NWA'));
-console.log(divideByThree("live"));
-console.log(divideByThree('Commander'));
-console.log(divideByThree('Commander commander'));
+//console.log(divideByThree('NWA'));
+//console.log(divideByThree("live"));
+//console.log(divideByThree('Commander'));
+//console.log(divideByThree('Inspector Gadget'));
 
-
-
-
-
-/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
 //10
 
 
