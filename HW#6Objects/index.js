51ff75a82fe705d8d28d8e40255315655bcdbdb9
Anrdii <andnkt20@gmail.com>
2020-02-16 18:55:36 +0200

    init HM6

---
+++ HW#6Objects/index.js
@@ -0,0 +1,46 @@
+
+const students = [{
+  name: "Tanya",
+  course: 3,
+  subjects: {
+    math: [4, 4, 3, 4],
+    algorithms: [3, 3, 3, 4, 4, 4],
+    data_science: [5, 5, 3, 4]
+  }
+}, {
+  name: "Victor",
+  course: 4,
+  subjects: {
+    physics: [5, 5, 5, 3],
+    economics: [2, 3, 3, 3, 3, 5],
+    geometry: [5, 5, 2, 3, 5]
+  }
+}, {
+  name: "Anton",
+  course: 2,
+  subjects: {
+    statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+    english: [5, 3],
+    cosmology: [5, 5, 5, 5]
+  }
+}];
+
+////////////////////////////////////////////////////////////////////////////////////////
+//1
+let getSubjects = (stud) =>{
+
+	let {subjects} = stud;
+	return Object.keys(subjects).map(el=> el.charAt(0).toUpperCase().concat(el.slice(1)).replace('_', ' '));
+
+}
+
+
+console.log(getSubjects( students[0]));
+
+////////////////////////////////////////////////////////////////////////////////////////
+//2
+
+//let getStudentInfo(studs) => {
+//}
+
+//	console.log(getSubjects(students));
