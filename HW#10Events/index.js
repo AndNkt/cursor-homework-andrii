e3e2301c50fd4bae5d26e4b4a1a1d2eee41a3817
Anrdii <andnkt20@gmail.com>
2020-03-10 19:54:29 +0200

    refactorHM#10 stage4

--- HW#10Events/index.js
+++ HW#10Events/index.js
@@ -2,12 +2,43 @@
 
 window.onload = function(){
 
+	generateBlocks();
 
 	document.addEventListener('keydown', (evnt)=>{ playMusic(evnt)});
 
 	document.addEventListener('keyup', (evnt)=>{delSelect(evnt)});
 
-	generateBlocks();
+
+	//adding listener to on click to div>span+kbd with attr [name]
+	document.addEventListener('mousedown', (evnt)=>{
+		//console.log('tag',event.target.tagName);
+
+		//let selElement = event.target.tagName;
+
+		if(evnt.target.getAttribute("name")){
+			//console.log('key-', evnt.target.getAttribute("name"));
+			let selectedKey = evnt.target.getAttribute("name");
+			
+			let selectAudio = document.querySelector(`audio[data-key=${selectedKey}]`);
+
+			let selectedDiv=document.querySelector(`div[name=${selectedKey}]`);
+			selectedDiv.classList.add('selected');
+			
+			selectAudio.currentTime = 0;
+			selectAudio.play();
+		}
+		//console.log(evnt.target.getAttributeNames())
+	});
+
+
+
+	document.addEventListener('mouseup', (evnt)=>{
+		let allDivs = document.querySelectorAll('div.row>div');
+		//console.log(allDivs);
+		//bad Practice!! remove selected class from all div's
+		allDivs.forEach((el)=> el.classList.remove('selected'));
+	});
+
 
 	function delSelect(evnt){
 		//get btn unpressed 
@@ -55,9 +86,11 @@ function generateBlocks(){
 	 	parent.classList.add('col-sm');
 	 	parent.classList.add('mx-1');
 		parent.setAttribute("name",`${key}`);
-		parent.innerHTML = `<kbd>${key}</kbd>
-							<span>${val}</span>`;
+		parent.innerHTML = `<kbd name="${key}">${key}</kbd>
+							<span name="${key}">${val}</span>`;
 
+		/*
+		//BAD PRACTICE DON'T DO that at home!!!!!!!
 		//another inefficient way, create element with event listener (just for study purpose)
 		//adding event listener
 		parent.addEventListener('mousedown', function(){
@@ -71,6 +104,7 @@ function generateBlocks(){
 			parent.classList.remove('selected');
 		})
 
+		*/
 							
 		wrapper.appendChild(parent);
 
