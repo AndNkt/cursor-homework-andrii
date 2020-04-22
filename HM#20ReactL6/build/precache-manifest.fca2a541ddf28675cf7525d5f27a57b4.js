1bff740be89a254b81d1d1e8a45bc25653a59dbf
Anrdii <andnkt20@gmail.com>
2020-04-22 22:17:05 +0300

    refactor 2

---
+++ HM#20ReactL6/build/precache-manifest.fca2a541ddf28675cf7525d5f27a57b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc815bc0143a719c4259a9055257774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9586c702e4eb13608136",
+    "url": "/static/css/main.30d23351.chunk.css"
+  },
+  {
+    "revision": "81ccf1bbf7a5b44758d9",
+    "url": "/static/js/2.3197399d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3197399d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9586c702e4eb13608136",
+    "url": "/static/js/main.8f48f170.chunk.js"
+  },
+  {
+    "revision": "76192c3676a4bc817c72",
+    "url": "/static/js/runtime-main.5c7ff1ab.js"
+  }
+]);
